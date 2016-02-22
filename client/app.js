f50a0930ffca45a9f194d12f6c9c9e74085ba4e1
Ashley Nguyen <a.nguyen020@gmail.com>
2016-02-22 11:46:09 -0800

    create login file with GitHub and LinkedIn buttons

--- client/app.js
+++ client/app.js
@@ -1,12 +1,24 @@
-var app = angular.module('codequest', ['ngRoute', 'ngResource']);
+var app = angular.module('codequest', ['ngRoute', 'ngResource', 'satellizer']);
 
-app.config(function($routeProvider){
+app.config(function($routeProvider, $authProvider, $locationProvider){
   $routeProvider
   .when('/', {
     templateUrl:'/client/templates/landingPage.html',
     controller:'IndexController'
   })
+  .when('/login', {
+    templateUrl: '/client/templates/login.html',
+    controller: 'LoginController'
+  })
   .otherwise({
     redirectTo: '/'
+  });
+
+  $authProvider.github({
+    clientId: 'ac8daf97790baadd5e1f'
+  });
+
+  $authProvider.linkedin({
+    clientId: '75hckaydg5jr06'
   });
 });
--- client/js/controllers.js
+++ client/js/controllers.js
@@ -4,3 +4,16 @@ var app = angular.module('codequest');
 app.controller('IndexController', function(){
   console.log('IndexController');
 });
+
+app.controller('LoginController', function($scope, $auth, $location) {
+  $scope.isAuthenticated = function() {
+    return $auth.isAuthenticated();
+  }
+
+  $scope.authenticate = function(provider) {
+    $auth.authenticate(provider)
+      .then(function() {
+        $location.path("/")
+      });
+  }
+});
