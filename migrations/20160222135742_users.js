42a87efa3ad1994e70ae4108ee9008a1a89156d3
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-02-22 14:15:13 -0800

    created users table

---
+++ migrations/20160222135742_users.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(table){
+    //users ID
+    table.increments();
+
+    table.string('name');
+    table.string('email');
+    table.string('password');
+    table.string('github');
+    table.string('twitter');
+    table.string('image');
+    table.string('position');
+
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
