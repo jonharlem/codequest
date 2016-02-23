de30e558da68e90bd33bf57ce36613a643d2aae8
HalahRaadSalih <hala.raad.salih@gmail.com>
2016-02-23 12:03:18 -0800

    added companies table

---
+++ migrations/20160223111728_companies.js
@@ -0,0 +1,21 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('companies', function(table){
+    table.increments();
+    table.string('name');
+    // for long text
+    table.text('description');
+    table.string('size');
+    table.string('image');
+    table.string('logo');
+    table.date('foundedDate');
+    // could be anything
+    table.text('contactInfo');
+    // something like b2b ..etc
+    table.string('industry');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('companies');
+};
